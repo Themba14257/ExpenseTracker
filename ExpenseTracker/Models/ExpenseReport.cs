1593c411304db0ad56f8048f9ab9b2be7e22f7d7
Themba14257 <thembanaterichard@gmail.com>
2023-01-18 21:06:40 +0200

    Expense Report Class added

--- ExpenseTracker/Models/ExpenseReport.cs
+++ ExpenseTracker/Models/ExpenseReport.cs
@@ -1,10 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExpenseTracker.Models
 {
     public class ExpenseReport
     {
         [Key]
+        public int itemId { get; set; }
+        [Required]
+        public string itemName { get; set; }
 
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal price { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [Required]
+        public DateTime expenseDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public string Category { get; set; }
+
+        
     }
 }
